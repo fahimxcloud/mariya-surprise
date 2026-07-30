2fb640289848516b4e6d189a929cbc32f82719bb
fahimxcloud <yktiykdkit@protonmail.com>
2026-07-30 17:45:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function openSurprise(){
+// Begin Surprise Button
 
-    document.querySelector(".hero").style.display = "none";
+const startBtn = document.getElementById("startBtn");
 
-    document.querySelector(".message").style.display = "flex";
+startBtn.addEventListener("click", () => {
 
-} 
+    document.querySelector(".gallery").scrollIntoView({
+        behavior: "smooth"
+    });
+
+});
+
+// Floating Particles
+
+const hero = document.querySelector(".hero");
+
+for(let i=0;i<40;i++){
+
+    let particle=document.createElement("span");
+
+    particle.className="particle";
+
+    particle.style.left=Math.random()*100+"%";
+
+    particle.style.animationDelay=Math.random()*8+"s";
+
+    particle.style.animationDuration=(6+Math.random()*6)+"s";
+
+    hero.appendChild(particle);
+
+}
